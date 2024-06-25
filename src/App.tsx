fbcafe66bc14214a08d4e1fbece0788a466d0549
gimminjae <min356812@naver.com>
2024-06-25 21:59:07 +0900

    chore: remove import unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import { useEffect } from 'react';
-import {BrowserRouter, Link, useLocation} from 'react-router-dom';
 
 import "preline/preline";
 import { IStaticMethods } from "preline/preline";
