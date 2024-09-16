442658c833128b6ee2468b9b90f467d76d3e7059
gimminjae <min356812@naver.com>
2024-09-16 17:12:45 +0900

    feat: bible select

--- src/components/bible/BibleViewPageHeader.tsx
+++ src/components/bible/BibleViewPageHeader.tsx
@@ -8,7 +8,7 @@ import List from "@mui/material/List"
 import ListItem from "@mui/material/ListItem"
 import ListItemButton from "@mui/material/ListItemButton"
 import ListItemText from "@mui/material/ListItemText"
-import { ButtonGroup } from "@mui/material"
+import { ButtonGroup, Grid, Paper, styled } from "@mui/material"
 import useBibleSearchParams from "../../store/zustand/BibleSearchParams"
 import { bibleInfos, getBookName } from "../../api/bible"
 import CancelIcon from "@mui/icons-material/Cancel"
@@ -27,10 +27,15 @@ function BibleViewPageHeader() {
   const [langState, setLangState] = useState({
     bottom: false,
   })
+  const [newSearchParam, setNewSearchParam] = useState({
+    bookCode: "",
+    chapter: 0,
+  })
 
   const toggleDrawer =
     (anchor: string, open: boolean) =>
     (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (open) setNewSearchParam({ bookCode: "", chapter: 0 })
       if (
         event &&
         event.type === "keydown" &&
@@ -58,35 +63,68 @@ function BibleViewPageHeader() {
       setLangState((prev) => ({ ...prev, [anchor]: open }))
     }
 
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    ...theme.applyStyles("dark", {
+      backgroundColor: "#1A2027",
+    }),
+  }))
+
   const list = (anchor: string) => (
     <Box
       sx={{ width: anchor === "bottom" ? "auto" : 250 }}
       role="presentation"
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      <List>
-        {bibleInfos?.map((info, index) => (
-          <ListItem key={info.bookCode} disablePadding>
-            <ListItemButton>
-              {!index ? (
-                <>
+      {!newSearchParam.bookCode && (
+        <List>
+          {bibleInfos?.map((info, index) => (
+            <ListItem key={info.bookCode} disablePadding>
+              <ListItemButton>
+                {!index ? (
+                  <>
+                    <ListItemText
+                      onClick={toggleDrawer(anchor, false)}
+                      className="text-center"
+                    >
+                      <CancelIcon />
+                    </ListItemText>
+                  </>
+                ) : (
                   <ListItemText
-                    onClick={toggleDrawer(anchor, false)}
-                    className="text-center"
-                  >
-                    <CancelIcon />
-                  </ListItemText>
-                </>
-              ) : (
-                <ListItemText
-                  onClick={(e) => console.log(e)}
-                  primary={getBookName(info.bookCode, searchParam.lang)}
-                />
-              )}
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+                    onClick={() =>
+                      setNewSearchParam((prev) => ({
+                        ...prev,
+                        bookCode: info.bookCode,
+                      }))
+                    }
+                    primary={getBookName(info.bookCode, searchParam.lang)}
+                  />
+                )}
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      )}
+      {newSearchParam.bookCode && (
+        <Grid container spacing={1}>
+          {Array.from(
+            {
+              length:
+                bibleInfos.find(
+                  (info) => info.bookCode === newSearchParam.bookCode
+                )?.maxChapter || 0,
+            },
+            (_, i) => i + 1
+          ).map((val, idx) => (
+            <Item key={idx}>{val}</Item>
+          ))}
+        </Grid>
+      )}
     </Box>
   )
   const langList = () => (
