a9cd93207f694ebe7b64769ca651bc34c7dac916
gimminjae <min356812@naver.com>
2024-10-11 15:47:28 +0900

    feat: apply copy button

--- src/components/global/ToggleButtonsMultiple.tsx
+++ src/components/global/ToggleButtonsMultiple.tsx
@@ -5,7 +5,10 @@ import ToggleButtonGroup from "@mui/material/ToggleButtonGroup"
 import ContentCopyIcon from "@mui/icons-material/ContentCopy"
 import FavoriteIcon from "@mui/icons-material/Favorite"
 import EditNoteIcon from "@mui/icons-material/EditNote"
-import { memo, useState } from "react"
+import { memo, useCallback, useState } from "react"
+import util from "../../util/util"
+import { makeCopyBibles } from "../../util/bible"
+import useSelectedContent from "../../store/zustand/SelectedContent"
 
 function ToggleButtonsMultiple() {
   const [formats, setFormats] = useState(() => ["bold", "italic"])
@@ -16,21 +19,29 @@ function ToggleButtonsMultiple() {
   ) => {
     setFormats(newFormats)
   }
+  const { content, emptyCopyText } = useSelectedContent()
+
+  const copy = useCallback(() => {
+    if (content?.copyText) util.copyContent(makeCopyBibles(content?.copyText))
+    emptyCopyText()
+  }, [content?.copyText])
 
   return (
     <ToggleButtonGroup
       className="animate-fade-up"
       value={formats}
+      exclusive
+      // color="secondary"
       onChange={handleFormat}
       aria-label="text formatting"
     >
-      <ToggleButton value="bold" aria-label="bold">
+      <ToggleButton value="heart" aria-label="heart">
         <FavoriteIcon />
       </ToggleButton>
-      <ToggleButton value="italic" aria-label="italic">
+      <ToggleButton value="memo" aria-label="memo">
         <EditNoteIcon />
       </ToggleButton>
-      <ToggleButton value="underlined" aria-label="underlined">
+      <ToggleButton value="copy" aria-label="copy" onClick={copy}>
         <ContentCopyIcon />
       </ToggleButton>
       <ToggleButton value="color" aria-label="color" disabled>
