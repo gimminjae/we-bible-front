f48a92e23aac4e46d080674822e9fcca383ac07b
gimminjae <min356812@naver.com>
2024-10-24 14:10:09 +0900

    refactor: useToast

--- src/hooks/useToast.tsx
+++ src/hooks/useToast.tsx
@@ -1,59 +1,50 @@
-import { VariantType, useSnackbar } from "notistack"
+import { useSnackbar } from "notistack"
+import { useCallback } from "react"
+
+interface Options {
+  autoHideDuration?: number
+  anchorOrigin?: {
+    vertical: "top" | "bottom"
+    horizontal: "left" | "center" | "right"
+    variant: "success" | "error" | "warning" | "info" | "default"
+  }
+}
 
 const useToast = () => {
   const { enqueueSnackbar } = useSnackbar()
-  const success = (message: string) => {
-    enqueueSnackbar(message, {
+  const getOptions = useCallback(
+    (options: any) => ({
       autoHideDuration: 2000,
       anchorOrigin: {
         vertical: "top",
         horizontal: "center",
       },
       variant: "success",
-    })
+      ...options,
+    }),
+    []
+  )
+  const success = (message: string) => {
+    enqueueSnackbar(message, getOptions({ variant: "success" }))
   }
   const error = (message: string) => {
-    enqueueSnackbar(message, {
-      autoHideDuration: 2000,
-      anchorOrigin: {
-        vertical: "top",
-        horizontal: "center",
-      },
-      variant: "error",
-    })
+    enqueueSnackbar(message, getOptions({ variant: "error" }))
   }
   const warning = (message: string) => {
-    enqueueSnackbar(message, {
-      autoHideDuration: 2000,
-      anchorOrigin: {
-        vertical: "top",
-        horizontal: "center",
-      },
-      variant: "warning",
-    })
+    enqueueSnackbar(message, getOptions({ variant: "warning" }))
   }
   const info = (message: string) => {
-    enqueueSnackbar(message, {
-      autoHideDuration: 2000,
-      anchorOrigin: {
-        vertical: "top",
-        horizontal: "center",
-      },
-      variant: "info",
-    })
+    enqueueSnackbar(message, getOptions({ variant: "info" }))
   }
   const defaultToast = (message: string) => {
-    enqueueSnackbar(message, {
-      autoHideDuration: 2000,
-      anchorOrigin: {
-        vertical: "top",
-        horizontal: "center",
-      },
-      variant: "default",
-    })
+    enqueueSnackbar(message, getOptions({ variant: "default" }))
   }
   return {
     success,
+    error,
+    warning,
+    info,
+    defaultToast,
   }
 }
 
